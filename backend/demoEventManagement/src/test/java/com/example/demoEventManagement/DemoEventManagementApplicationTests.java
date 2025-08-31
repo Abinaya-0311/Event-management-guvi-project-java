ab4469a0574854fceb29f5d270ae7ffbce461fc8
JJ <vsjayaraj407@gmail.com>
2025-08-31 18:48:09 +0530

    Initial setup for backend and frontend

---
+++ backend/demoEventManagement/src/test/java/com/example/demoEventManagement/DemoEventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoEventManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
